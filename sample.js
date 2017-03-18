819da61fe6c5099d5c5153169bd9441eb6558c4e
imran <imran@gmail.com>
2017-03-18 16:28:27 +0530

    msg3

--- sample.js
+++ sample.js
@@ -5,13 +5,13 @@ server.use(bodyParser());
 var homeListener = function(req,res){
   res.send('hello you , welcome to homeListener')
 };
-server.get('/',homeListener);
+server.get('/home',homeListener);
 server.get('/search', function(req,res){
  console.log(req.query);	
  var in_var = Number(req.query.var1) ;
  res.json({ok:true,message:'Square is '+(in_var*in_var)})
-})
-
+});
+server.use("/", express.static('public'));
 server.listen(8000,function(){
 	console.log('Listening on localhost:8000')
-})
+});
